4306072c08d2b0533fac7e3d9eb99ee070181b39
kartiks2000 <46376692+kartiks2000@users.noreply.github.com>
2020-07-20 20:01:51 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React,{ Component } from 'react';
+import './App.css';
+import Counter from './component/counter';
+
+function App() {
+  return (
+    <div className="App">
+      <Counter></Counter>
+    </div>
+  );
+}
+
+export default App;
